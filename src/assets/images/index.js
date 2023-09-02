59acc4c15d07566352bdce3528b5e9420d0f90da
RoyceGhub <roycetecson@gmail.com>
2023-09-02 16:46:15 +0800

    test image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,2 +1,4 @@
 export { default as careersImage } from './careers.png';
 export { default as heroImage } from './hero.png';
+export { default as servicesImage1 } from './leftsideservicespage.png'
+export { default as servicesImage2 } from './rightsideservicespage.png'
--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -1,5 +1,18 @@
+import { servicesImage1, servicesImage2 } from "../../assets/images";
+
 const Service = () => {
-    return <section className="container mx-auto">Service</section>;
+    return <section className="container mx-auto">
+        <div className="flex justify-between p-5">
+
+        <div>
+        <img src={servicesImage1} alt="" />
+        </div>
+        <div>
+
+        <img src={servicesImage2} alt="" />
+        </div>
+        </div>
+    </section>;
 };
 
 export default Service;
